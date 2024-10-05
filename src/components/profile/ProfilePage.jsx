6915223ee7dc017081282ef54b169329cc1c4476
Marin Marinov <marinm309@abv.bg>
2024-10-05 17:12:20 +0300

    asdf

--- src/components/profile/ProfilePage.jsx
+++ src/components/profile/ProfilePage.jsx
@@ -2,25 +2,27 @@ function ProfilePage(){
     return(
         <div className="profile-container">
             <div className="profile-card">
-                <div className="profile-image">
-                    <input type="file" id="file-upload" className="file-upload" accept="image/*" />
-                    <label htmlFor="file-upload" className="upload-btn">
-                        <i className="fa fa-camera"></i>
-                    </label>
+                <div className="profile-card-inner-container">
+                    <div className="profile-image">
+                        <input type="file" id="file-upload" className="file-upload" accept="image/*" />
+                        <label htmlFor="file-upload" className="upload-btn">
+                            <i className="fa fa-camera"></i>
+                        </label>
+                    </div>
+                    <h2>Потребител</h2>
+                    <p><i className="fa fa-user"></i> Потребител от 3 февруари 2023</p>
+                    <p><i className="fa-solid fa-location-dot"></i> Варна, България</p>
+                    <p className="listings">1000 обяви</p>
                 </div>
-                <h2>Потребител</h2>
-                <p><i className="fa fa-user"></i> Потребител от 3 февруари 2023</p>
-                <p><i className="fa fa-map-marker"></i> Варна, България</p>
-                <p className="listings">1000 обяви</p>
             </div>
             <div className="review">
                 <h3>Ревю от други потребители</h3>
                 <div className="stars">
-                    <span className="star filled">★</span>
-                    <span className="star filled">★</span>
-                    <span className="star filled">★</span>
-                    <span className="star filled">★</span>
-                    <span className="star empty">★</span>
+                    <span className="star empty"><i class="fa-regular fa-star"></i></span>
+                    <span className="star filled"><i class="fa-solid fa-star"></i></span>
+                    <span className="star filled"><i class="fa-solid fa-star"></i></span>
+                    <span className="star filled"><i class="fa-solid fa-star"></i></span>
+                    <span className="star filled"><i class="fa-solid fa-star"></i></span>
                 </div>
             </div>
         </div>
